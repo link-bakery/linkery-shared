bb8781dafbf557811fc771351f54df7d9359bf55
Madu-de <kontakt.madu@gmail.com>
2025-06-01 15:20:03 +0200

    Move to index.ts and change to commonjs

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-export * from './redirect';
+import { z } from 'zod';
+
+export const RedirectSchema = z.strictObject({
+  path: z.string().min(1),
+  redirectTo: z.string().min(1),
+});
+
+export type Redirect = z.infer<typeof RedirectSchema>;
